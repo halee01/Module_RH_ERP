60707590f10963ccb028c10a6b7e18494d0915ee
halee01 <halee.bk01@gmail.com>
2023-04-27 08:54:22 +0100

    4-27 first

--- CSI-Dashboard/src/app/views/Component/HumanResource/candidate/CandidatCrud/Candidat-details/candidat-details.component.ts
+++ CSI-Dashboard/src/app/views/Component/HumanResource/candidate/CandidatCrud/Candidat-details/candidat-details.component.ts
@@ -4,6 +4,11 @@ import { ActivatedRoute } from '@angular/router';
 import { Partner } from 'app/shared/models/Partner';
 import { CrudService } from '../candidat-crud.service';
 import { Employee } from 'app/shared/models/Employee';
+import { Education } from 'app/shared/models/Education';
+import { Skills } from 'app/shared/models/Skills';
+import { SkillsCategory } from 'app/shared/models/SkillsCategory';
+import { Certification } from 'app/shared/models/Certification';
+import { Experience } from 'app/shared/models/Experience';
 @Component({
   selector: 'app-details-candidat',
   templateUrl: './candidat-details.component.html'
@@ -11,6 +16,11 @@ import { Employee } from 'app/shared/models/Employee';
 export class CandidatDetailComponent implements OnInit {
 id: number
 employee : Employee
+education : Education
+skills : Skills
+skillsCategory : SkillsCategory
+certification : Certification
+experience : Experience
   constructor(    private route: ActivatedRoute,
     private candidatService: CrudService,) { }
 
